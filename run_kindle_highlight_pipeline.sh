cf74c9d865767c2f464c49df63bb3bae0ed83046
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-01-08 12:53:30 +0100

    Don't install requirements on virtualenv

--- run_kindle_highlight_pipeline.sh
+++ run_kindle_highlight_pipeline.sh
@@ -4,8 +4,9 @@ source set_kindle_password_environment_variable.sh
 ruby fetch_kindle_highlights.rb > highlights.txt
 
 source wordenv/bin/activate
-pip install -r kindle-requirements.txt
 python filter_words.py > words.txt
+awk '{if (NF == 1) print tolower($1)}' highlights.txt | awk '{gsub(/[[:punct:]]/, "")} 1' > words_awk.txt
+# should be exactly the same, but this also includes '-'
 DATE=`date +%Y-%m-%d`
 N=`python write_words_to_airtable.py`
 echo "$DATE $N" >> cron_log.txt
